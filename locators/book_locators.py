0efae725d6d89d1f4ecaba1adfdb3873ca8e304c
Thulasi, Raman <thulasirmn8@gmail.com>
2018-06-21 23:46:15 +0530

    Second commit

--- locators/book_locators.py
+++ locators/book_locators.py
@@ -3,6 +3,7 @@ class Book_locators:
     LINK_LOCATOR = 'article.product_pod h3 a'
     PRICE_LOCATOR = 'article.product_pod p.price-color'
     RATING_LOCATOR = 'article.product_pod p.star-rating'
+    ATTRIBUTE_LOCATOR = ''
 
     
 
